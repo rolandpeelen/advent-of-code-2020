aefa7446127af0b5a145054a37bee319e353f198
Roland Peelen <roland@peppr.nl>
2020-12-01 21:28:26 +0100

    :art: - Remove comments in favor of Readme

--- 1b.hs
+++ 1b.hs
@@ -3,20 +3,8 @@ import Prelude
 
 main = do
   input <- getContents
-  -- Take the input,
-  -- convert it to lines, then read to get ints
-  -- then sort the list
   putStr $ show $ find2020 (-1) (sort $ map read $ lines input)
 
--- This algoritm works with a sorted array. It pins the first item, then 
--- 'findSecond' takes over, which checks wether adding that first item,
--- the head of the rest, and the tail of the list is equal bigger or smaller 
--- than 2020. 
--- If its equals 2020, we've found our hit.
--- If it's smaller, it needs to call itself again with the tail of the list
--- If it's bigger, it needs to call itself again with everything apart form the 
--- tail of the list
--- It works because it's sorted...
 find2020 :: Int -> [Int] -> Int
 find2020 _ [] = -1
 find2020 (-1) (x : xs) = find2020 x xs
