e22d2e1db62163c13024c70059d9e41a6d43ffdf
Roland Peelen <roland@peppr.nl>
2020-12-01 20:49:26 +0100

    :recycle: - Use curried versions, less parenthesis

--- 1a.hs
+++ 1a.hs
@@ -5,7 +5,7 @@ main = do
   input <- getContents -- Take the contents and push that inputs
   -- take input, map it to lines of input, map that with read (so we get Ints)
   -- run that through the find2020, then show (maps it back to String)
-  putStr $ show $ find2020 $ (map read $ lines input, [])
+  putStr $ show $ find2020 (map read $ lines input) []
 
 -- Find2020 takes two lists, the input, and an intermediate second list, 
 -- if it encounters an emtpy list on the left hand side, and we haven't returned
@@ -13,9 +13,9 @@ main = do
 -- If we have a list, take the head, check if we can find an item we've already
 -- seen with which it makes a pair. If so, return the product, if not, call
 -- find2020 again with the head appended to the items we've already seen.
-find2020 :: ([Int], [Int]) -> Int
-find2020 ([], _) = -1
-find2020 (head:xs, ys) =
-  case (find (\x -> x == 2020 - head) ys) of
+find2020 :: [Int] -> [Int] -> Int
+find2020 [] _ = -1
+find2020 (head:xs) ys =
+  case (find (\x -> x + head == 2020) ys) of
     Just x -> x * head
-    Nothing -> find2020 (xs, ys ++ [head])
+    Nothing -> find2020 xs $ ys ++ [head]
