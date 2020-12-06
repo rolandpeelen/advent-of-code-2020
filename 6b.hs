51918fc9e3d13178950a312c617445db422d09e2
Roland Peelen <roland@peppr.nl>
2020-12-06 12:45:08 +0100

    :art: - Found answer two

---
+++ 6b.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Prelude
+import Data.List
+
+magicRowPartitionLength = 7
+
+main = do
+  input <- getContents
+  putStr $ show $ fn $ input
+
+groupAnswers :: [String] -> [[ String ]]
+groupAnswers xs = map (filter ((/=) "")) $ groupBy(\_ y -> y /= "") xs
+
+mergeAnswers :: [[String]] -> [(Int, String)]
+mergeAnswers xs = map (\ys -> (length ys, unwords ys)) xs
+
+checkAnswer :: (Int, String) -> Int
+checkAnswer (count, answers) = length $ filter (count==) $ map (\x -> (length $ filter (x==) answers)) $ nub answers 
+
+--fn :: [String] -> [ C ]
+fn xs = sum $ map checkAnswer $ mergeAnswers $ groupAnswers $ lines xs
