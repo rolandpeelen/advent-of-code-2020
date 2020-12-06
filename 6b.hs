430316a1608663cf3863dd381cd7e6c62c755ea4
Roland Peelen <roland@peppr.nl>
2020-12-06 13:09:48 +0100

    :bulb: - Add some comments to otherwise abstract fn

--- 6b.hs
+++ 6b.hs
@@ -11,8 +11,16 @@ groupAnswer xs = map (filter ((/=) "")) $ groupBy (\_ y -> y /= "") xs
 mergeAnswer :: [String] -> (Int, String)
 mergeAnswer x = (\ys -> (length ys, unwords ys)) x
 
+countOccurances :: Eq a => [a] -> a -> Int
+countOccurances xs y = length $ filter (y==) xs
+
+-- This does, right to left:
+-- $ create a list of unique items from the answers
+-- $ map the unique items, replace each of them with the count of them in the original list
+-- $ filter where the count is equal to the amount of group members (ie. 3 people answered, filter the items where all 3 did for that specific answer)
+-- check length 
 checkAnswer :: (Int, String) -> Int
-checkAnswer (count, answers) = length $ filter (count ==) $ map (\x -> (length $ filter (x ==) answers)) $ nub answers
+checkAnswer (count, answers) = length $ filter (count ==) $ map (countOccurances answers) $ nub answers
 
 fn :: String -> Int
 fn xs = sum $ map (checkAnswer . mergeAnswer) $ groupAnswer $ lines xs
