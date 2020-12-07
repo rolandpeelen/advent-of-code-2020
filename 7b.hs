e6c2d360656aac359820d18e88130240cacd69dc
Roland Peelen <roland@peppr.nl>
2020-12-07 20:04:40 +0100

    :bulb: - Added some comments for clarity

--- 7b.hs
+++ 7b.hs
@@ -43,18 +43,43 @@ bagParser = do
 addToCount :: BagCount -> [InnerBag] -> Int
 addToCount acc xs = foldl (\acc x -> (fst x) + acc) acc xs
 
+-- Given a list of Outerbags, filter them by name
 filterBag :: BagName -> [OuterBag] -> [OuterBag]
 filterBag bagName = filter (\y -> (fst y) == bagName)
 
+-- Multiple the amount of the bags that can be inside one other bag
+-- by the parent bag. Multiplying because if we have 10 bags, that all can have
+-- 5 other bags in which our bag could go, then we would have 10 * 5 = 50 bags
+-- in which to put ours.
+-- ie. 10 [(5, "b"), (3, "c")] where 10 is the amount of bags in the parent
+-- to [(50, "b"), (30, "c")]
 multiplyCountBy :: BagCount -> [InnerBag] -> [InnerBag]
 multiplyCountBy count = map (\y -> (((fst y) * count), snd y))
 
+-- This takes a list of outer bags and an innerbag,
+-- [("a", [(4, "b"),(12, "c")]), ("e", [(2, "f"), (4, "a")])] (12, "e")
+-- Filters them (filtered by the name of some other bag)
+-- [("e", [(2, "f"), (4, "a")])]
+-- Takes the list of bags that's inside of that bag
+-- [[(2, "f"), (4, "a")]]
+-- Concats that
+-- [[(2, "f"), (4, "a")]]
+-- Multiplies every item by the initial occurances
+-- [[(2, "f"), (4, "a")]] 12
+-- [[(24, "f"), (48, "a")]] 12
 countBags :: [OuterBag] -> InnerBag -> [InnerBag]
 countBags xs (occurances, bagName) = multiplyCountBy occurances $ concat $ map snd $ filterBag bagName xs
 
+-- Given a list of Inners, map them to the structure above
+-- [(12, "e"), (3, "a")]
+-- [[(12, "b"), (36, "c")], [(24, "f"), (48, "a")]]
 findChildren :: [InnerBag] -> [OuterBag] -> [InnerBag]
 findChildren xs ys = concat $ map (countBags ys) xs
 
+-- A recursive wrapper to the one above, that takes the output, and for every
+-- iteration, add the amount of bags to the count
+-- [[(12, "b"), (36, "c")], [(24, "f"), (48, "a")]]
+-- count -> 12 + 36 + 24 + 48
 findChildrenRec :: Int -> [InnerBag] -> [OuterBag] -> Int
 findChildrenRec acc xs ys = case (findChildren xs ys) of
   [] -> acc
