2c7cb1b0a2cbd89b8fbd874229642c08f9972525
Roland Peelen <roland@peppr.nl>
2020-12-09 19:46:55 +0100

    :art: - Found solution a

---
+++ 9a.hs
@@ -0,0 +1,23 @@
+import Data.List
+import qualified Data.Set as S
+import Prelude
+
+main = do
+  input <- getContents
+  putStr $ show $ fn $ lines $ input
+
+sumsToSet :: [Int] -> S.Set Int
+sumsToSet xs = S.fromList $ [a + b | a <- xs, b <- xs]
+
+findMissing :: S.Set Int -> Int -> Maybe Int
+findMissing xs y = case (S.member y xs) of
+  True -> Nothing
+  False -> Just y
+
+findAllMissing :: [Int] -> Int
+findAllMissing [] = -1
+findAllMissing xs = case (\x -> findMissing (sumsToSet $ fst x) (head $ snd x)) $ splitAt 25 xs of
+  Nothing -> findAllMissing (drop 1 xs)
+  Just x -> x
+
+fn xs = findAllMissing $ map (read :: String -> Int) xs
