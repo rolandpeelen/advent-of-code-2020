0915eec252f273f63f1602b0917115b6aa2fad1f
Roland Peelen <roland@peppr.nl>
2021-04-14 22:19:37 +0200

    :recycle: - Actually use infite data structure as opposed to looking up by index

--- 1b.hs
+++ 1b.hs
@@ -9,13 +9,14 @@ main = do
 filterChars :: String -> String -> String
 filterChars xs = filter (not . (`elem` xs))
 
-findNo :: [Int] -> Int -> Int -> [Int] -> Int
-findNo seen count idx input = case nextSeen of
+findNo :: [Int] -> Int -> [Int] -> Int
+findNo seen count (current : rest) = case seenCount' of
   Just no -> no
-  Nothing -> findNo (current : seen) current (idx + 1) input
+  Nothing -> findNo seen' count' rest
   where
-    current = count + (input !! idx)
-    nextSeen = find (== current) seen
+    seenCount' = find (== count') seen
+    count' = count + current
+    seen' = count' : seen
 
 fn :: [Int] -> Int
-fn xs = findNo [] 0 0 (cycle xs)
+fn xs = findNo [] 0 (cycle xs)
